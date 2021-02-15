e8cf11e0e5fe4c8e355ab8ab95696bb795d7f9eb
clive3 <cneil@noc.ac.uk>
2021-02-15 19:34:06 +0000

    various

--- coastsat/SDS_tools.py
+++ coastsat/SDS_tools.py
@@ -60,8 +60,8 @@ def convert_pix2world(points, georef):
             points_converted.append(tform(tmp))
           
     # if single array
-#    elif type(points) is np.ndarray:
-    elif True:
+    elif type(points) is np.ndarray:
+#    elif True:
         tmp = points[:,[1,0]]
         points_converted = tform(tmp)
         
@@ -566,13 +566,21 @@ def polygon_from_kml(fn):
     str1 = '<coordinates>'
     str2 = '</coordinates>'
     subdoc = doc[doc.find(str1)+len(str1):doc.find(str2)]
-    coordlist = subdoc.split(',')
+    coordlist = subdoc.split('\n')
+
+#    coordlist = subdoc.split(',')
+#    # read coordinates
+#    polygon = []
+
+#    for coord_pair in range(int(len(coordlist)/2)):
+#        lat = float(coordlist[coord_pair*2].replace('0 ', ''))
+#        lon = float(coordlist[coord_pair*2+1].replace('0 ', ''))
+#        polygon.append([lat, lon])
+
     # read coordinates
     polygon = []
-    for coord_pair in range(int(len(coordlist)/2)):
-        lat = float(coordlist[coord_pair*2].replace('0 ', ''))
-        lon = float(coordlist[coord_pair*2+1].replace('0 ', ''))
-        polygon.append([lat, lon])
+    for i in range(1, len(coordlist) - 1):
+        polygon.append([float(coordlist[i].split(',')[0]), float(coordlist[i].split(',')[1])])
 
         
     return [polygon]
