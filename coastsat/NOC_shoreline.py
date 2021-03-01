6e67dd6df291d1b294382eddf31c257cfc49106d
clive3 <kliveneil@yahoo.co.uk>
2021-03-01 17:14:47 +0000

    Update NOC_shoreline.py

--- coastsat/NOC_shoreline.py
+++ coastsat/NOC_shoreline.py
@@ -249,7 +249,7 @@ def adjust_detection_optical(image_ms, cloud_mask, image_labels, image_ref_buffe
     sl_plot3 = ax3.plot(sl_pix[:, 0], sl_pix[:, 1], 'k.', markersize=3)
     t_line = ax4.axvline(x=t_mndwi, ls='--', c='k', lw=1.5, label=f'threshold')
     thresh_label = ax4.text(t_mndwi+bin_width, 4, str(f'{t_mndwi:4.3f}'), rotation=90)
-    ax4.axvline(x=reference_threshold, ls='--', c='r', lw=1.5, label=f'ref threshold {reference_threshold:4.3f}')
+    ax4.axvline(x=t_mndwi, ls='--', c='r', lw=1.5, label=f'ref threshold {t_mndwi:4.3f}')
 
     ax4.legend(loc=1)
     plt.draw()  # to update the plot
